a3eb7ab1aab28269f938c7d45451a8288a9f1fa6
Yoel Jimenez del Valle <yoel07@gmail.com>
2020-04-09 18:12:28 -0400

    added a function to show tittle and image in navigation Controller

--- cuauhtemoc/controller/utils/UIViewControllerExtension.swift
+++ cuauhtemoc/controller/utils/UIViewControllerExtension.swift
@@ -15,4 +15,34 @@ extension UIViewController{
             self.present(alert, animated: true)
         }
     }
+    
+    func navTitleWithImageAndText(titleText: String, imageName: String) -> UIView {
+
+        // Creates a new UIView
+        let titleView = UIView()
+
+        // Creates a new text label
+        let label = UILabel()
+        label.text = titleText
+        label.sizeToFit()
+        label.center = titleView.center
+        label.textAlignment = NSTextAlignment.center
+        let image = UIImageView()
+        image.image = UIImage(named: imageName)
+        let imageAspect = image.image!.size.width / image.image!.size.height
+        let imageX = label.frame.origin.x - label.frame.size.height * imageAspect
+        let imageY = label.frame.origin.y
+        let imageWidth = label.frame.size.height * imageAspect
+        let imageHeight = label.frame.size.height
+        image.frame = CGRect(x: imageX, y: imageY, width: imageWidth, height: imageHeight)
+        image.contentMode = UIView.ContentMode.scaleAspectFit
+        titleView.addSubview(label)
+        titleView.addSubview(image)
+
+        // Sets the titleView frame to fit within the UINavigation Title
+        titleView.sizeToFit()
+
+        return titleView
+
+    }
 }
